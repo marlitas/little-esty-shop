f6af6ab8421c6fd0e3a61093d8bbbc42bc26c44d
danembb <dbrophy720@gmail.com>
2021-08-02 15:58:28 -0600

    Complete merchant item create user story

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,8 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    # require "pry"; binding.pry
     @item.update(item_params)
-
-    redirect_to "/merchants/#{@item.merchant_id}/items/#{@item.id}"
+    redirect_to "/merchants/#{@item.merchant_id}/items/#{@item.id}", notice: "Item Successfully Updated!"
   end
 
   #private makes it so that this method cannot be called by other classes.
