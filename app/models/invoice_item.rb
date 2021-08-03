34616857d5afc2f09352e5ed00decc0d22f74a1c
sami-p <sami.peterson14@gmail.com>
2021-08-03 17:35:06 -0500

    Add unit price to dollars conversion method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,8 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
 
   enum status: [:pending, :packaged, :shipped]
+
+  def unit_price_to_dollars
+    self.unit_price / 100.00
+  end
 end
