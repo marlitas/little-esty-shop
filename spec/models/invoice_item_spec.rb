06c3bf309f41ac3ee21226fa6ddc41fc632bbc4c
sami-p <sami.peterson14@gmail.com>
2021-08-04 19:08:24 -0500

    Test unit price to dollars mwithout argument method

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -21,5 +21,15 @@ RSpec.describe InvoiceItem, type: :model do
       expect(ii.price_to_dollars(item.unit_price)).to eq(20.50)
       expect(ii.price_to_dollars(ii.unit_price)).to eq(20.80)
     end
+
+    it 'can manipulate price to dollars without argument' do
+      merchant_1 = Merchant.create!(name: 'The Wand Shop')
+      item = merchant_1.items.create!(name: 'Horse Feather Oak', description: 'So magical', unit_price: 20000)
+      customer = Customer.create!(first_name: 'Harry', last_name: 'Potter')
+      invoice = Invoice.create!(customer_id: customer.id, status: 0)
+      ii = InvoiceItem.create!(item_id: item.id, invoice_id: invoice.id, status: 1, unit_price: 20000, quantity: 1)
+
+      expect(ii.unit_price_to_dollars).to eq(200.00)
+    end
   end
 end
