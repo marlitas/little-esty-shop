e7ec023df13493f5dd4e142dbb41e39cbca7beca
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-05 16:26:30 -0600

    Add discount relationship

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :discounts
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
@@ -43,5 +44,5 @@ class Merchant < ApplicationRecord
   def items_on_invoice(id)
     invoice_items.select('items.name, items.unit_price as item_price, invoice_items.*').where('invoice_id = ?', id)
   end
-  
+
 end
