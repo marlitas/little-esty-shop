ec3b95a9343bea1e2a019646eeb46a3eb97ad3bb
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-04 18:14:08 -0600

    Limit contributors

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe 'Application View' do
     it 'displays repo name' do
       visit "/admin"
 
-      save_and_open_page
-      
       expect(page).to have_content('little-esty-shop')
       expect(page).to have_content('marlita')
       expect(page).to have_content('Jtpiland')
