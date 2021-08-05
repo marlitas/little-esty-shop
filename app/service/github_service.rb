76dddd8fdbc20f49e2530ae35e8b983dc20555bf
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-04 18:11:23 -0600

    Integrate github API calls

--- app/service/github_service.rb
+++ app/service/github_service.rb
@@ -4,6 +4,16 @@ class GithubService
     get_data(endpoint)
   end
 
+  def contributor
+    endpoint = "https://api.github.com/repos/marlitas/little-esty-shop/contributors"
+    get_data(endpoint)
+  end
+
+  def pull_count
+    endpoint = "https://api.github.com/repos/marlitas/little-esty-shop/pulls?state=all"
+    get_data(endpoint)
+  end
+
   def get_data(endpoint)
     response = Faraday.get(endpoint)
     JSON.parse(response.body, symbolize_names: true)
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe 'Application View' do
   describe 'visitor' do
     it 'displays repo name' do
       visit "/admin"
+
+      save_and_open_page
+      
       expect(page).to have_content('little-esty-shop')
+      expect(page).to have_content('marlita')
+      expect(page).to have_content('Jtpiland')
+      expect(page).to have_content('sami-p')
+      expect(page).to have_content('danembb')
     end
   end
 end
