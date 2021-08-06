8b5f09b6c785534e868f4b621c0abb5beb3fdaef
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-06 16:34:01 -0600

    Add note

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,6 +2,7 @@ class Merchant::DiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     json = NagerService.new.get_holiday
+    #add method to filter holidays by three coming up
     @holidays = json.map do |holiday|
       Holiday.new(holiday)
     end
