47b84aa2b98e3ef723e870a53d06c5187651eade
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-07 16:20:05 -0600

    Refactor to factorybot

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
     association :customer, factory: :customer
   end
 
-  factory :cancelled_invoice do
+  factory :cancelled_invoice, class: Invoice do
     status { 0 }
     association :customer, factory: :customer
   end
 
-  factory :completed_invoice do
+  factory :completed_invoice, class: Invoice do
     status { 2 }
     association :customer, factory: :customer
   end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :transaction do
     credit_card_number {Faker::Stripe.valid_card}
-    credit_card_number_expiration_date {'02/04'}
+    credit_card_expiration_date {'02/04'}
     association :invoice, factory: :invoice
     result {0}
   end
 
-  factory :failed_transaction do
+  factory :failed_transaction, class: Transaction do
     credit_card_number {Faker::Stripe.valid_card}
-    credit_card_number_expiration_date {'02/04'}
+    credit_card_expiration_date {'02/04'}
     association :invoice, factory: :invoice
     result {1}
   end
