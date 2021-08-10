2164a4734bc25dfecc56db4cd9cc761998789af0
Marla Schulz <marlaa.schulz@gmail.com>
2021-08-10 09:36:59 -0600

    Remove unecessary method

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -11,18 +11,7 @@ RSpec.describe InvoiceItem, type: :model do
   end
 
   describe 'instance methods' do
-    it 'can transform unit price to dollars' do
-      merchant1 = Merchant.create!(name: 'Sparkys Shop')
-      item = merchant1.items.create!(name: 'Teddy Bear', description: 'So fuzzy', unit_price: 2050)
-      customer = Customer.create!(first_name: 'Sally', last_name: 'Sal')
-      invoice = Invoice.create!(customer_id: customer.id, status: 0)
-      ii = InvoiceItem.create!(item_id: item.id, invoice_id: invoice.id, status: 1, unit_price: 2080, quantity: 1)
-
-      expect(ii.price_to_dollars(item.unit_price)).to eq(20.50)
-      expect(ii.price_to_dollars(ii.unit_price)).to eq(20.80)
-    end
-
-    it 'can manipulate price to dollars without argument' do
+    it 'can manipulate price to dollars' do
       merchant_1 = Merchant.create!(name: 'The Wand Shop')
       item = merchant_1.items.create!(name: 'Horse Feather Oak', description: 'So magical', unit_price: 20000)
       customer = Customer.create!(first_name: 'Harry', last_name: 'Potter')
